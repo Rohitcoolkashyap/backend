959074dd2bbb36c09b838e32f925bf1458917fa8
RohitcoolKashyap <rohit.cse18@chitkarauniversity.edu.in>
2020-07-17 22:56:20 +0530

    s

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require('cors')
 const app = express();
+var port = process.env.PORT || 5000;
 const tweetRoute = require('./routes/tweet')
 const connectDb = require('./utils/connectDb')
-
 app.use(cors()); // add cross orgin request enable
 app.use(express.json()) // middleware to pass incoming data
 
 app.use("/", tweetRoute);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("listening on port 5000");
 });
